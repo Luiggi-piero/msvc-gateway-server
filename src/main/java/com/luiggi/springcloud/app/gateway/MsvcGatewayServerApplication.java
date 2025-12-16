0c51f7ccb141db3cfb952bc76c5505abc1ce7849
Luiggi <luiggiyantas@gmail.com>
2025-12-16 11:01:42 -0500

    conectando con zipki

--- src/main/java/com/luiggi/springcloud/app/gateway/MsvcGatewayServerApplication.java
+++ src/main/java/com/luiggi/springcloud/app/gateway/MsvcGatewayServerApplication.java
@@ -2,12 +2,48 @@ package com.luiggi.springcloud.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.web.servlet.function.RouterFunction;
+// import org.springframework.web.servlet.function.ServerResponse;
+
+import reactor.core.publisher.Hooks;
+
+// import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+// import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+// import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+// import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+// import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
+// import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.stripPrefix;
 
 @SpringBootApplication
 public class MsvcGatewayServerApplication {
 
 	public static void main(String[] args) {
+		// Habilita la propagación automática del contexto de Reactor (incluyendo traceId y spanId) 
+        // antes de que se inicialice el contexto de Spring. 
+        // Esto permite que los logs muestren trazabilidad automáticamente.
+        Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(MsvcGatewayServerApplication.class, args);
 	}
+
+	// - Esta congifuracion se uso para mover lo referente a msvc-products del yml a este archivo
+	// sobre todo lo de circuitBreaker, porque no funciona Gateway MVC con circuitBreaker(con esto tampoco funciona)
+	// - El problema es que no reconoce msvc-products a pesar de que sea una ruta libre
+	// - Lo recomendable es usar Gateway Reactivo
+	/* @Bean
+	RouterFunction<ServerResponse> routerConfig(){
+		return route("msvc-products") // id del msvc, es el nombre del proyecto que se encuentra en el properties de msvc-products
+			.route(
+				path("/api/products/**"), // patron para acceder al msvc de productos
+				http()
+			)
+			.filter(lb("msvc-products")) // balanceo de carga con multiples instancias
+			.filter(circuitBreaker(config -> config // filtro circuit breaker para manejar errores
+				.setId("products") // nombre de la instancia del circuit breaker
+				.setStatusCodes("500") // cuando se abre el circuito? cuando ocurre un error 500
+				.setFallbackPath("forward:/api/items/5"))) // camino alternativo si falla
+			.before(stripPrefix(2)) // la ruta tiene 2 niveles/prefijos son: api e items, es otro tipo de filtro
+			.build();
+	} */
 
 }
--- src/main/java/com/luiggi/springcloud/app/gateway/security/SecurityConfig.java
+++ src/main/java/com/luiggi/springcloud/app/gateway/security/SecurityConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import reactor.core.publisher.Mono;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,35 +27,34 @@ import java.util.stream.Collectors;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception{
-        return http.authorizeHttpRequests(authz -> {
-            authz.requestMatchers("/authorized", "/logout").permitAll()
-            .requestMatchers(HttpMethod.GET, "/api/items", "/api/products", "/api/users").permitAll()
-            .requestMatchers(HttpMethod.GET, "/api/items/{id}", "/api/products/{id}", "/api/users/{id}").hasAnyRole("USER", "ADMIN")
-            .requestMatchers("/api/items/**", "/api/products/**", "/api/users/**").hasRole("ADMIN")
-            // .requestMatchers(HttpMethod.PUT,"/api/items/{id}", "/api/products/{id}", "/api/users/{id}").hasRole("ADMIN")
-            // .requestMatchers(HttpMethod.POST, "/api/items", "/api/products", "/api/users").hasRole("ADMIN")
-            // .requestMatchers(HttpMethod.DELETE, "/api/items/{id}", "/api/products/{id}", "/api/users/{id}").hasRole("ADMIN")
-            .anyRequest().authenticated(); // cualquier otra ruta debe ser con autenticacion
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception{
+        return http.authorizeExchange(authz -> {
+            authz.pathMatchers("/authorized", "/logout").permitAll()
+            .pathMatchers(HttpMethod.GET, "/api/items", "/api/products", "/api/users").permitAll()
+            .pathMatchers(HttpMethod.GET, "/api/items/{id}", "/api/products/{id}", "/api/users/{id}").hasAnyRole("USER", "ADMIN")
+            .pathMatchers("/api/items/**", "/api/products/**", "/api/users/**").hasRole("ADMIN")
+            // .pathMatchers(HttpMethod.PUT,"/api/items/{id}", "/api/products/{id}", "/api/users/{id}").hasRole("ADMIN")
+            // .pathMatchers(HttpMethod.POST, "/api/items", "/api/products", "/api/users").hasRole("ADMIN")
+            // .pathMatchers(HttpMethod.DELETE, "/api/items/{id}", "/api/products/{id}", "/api/users/{id}").hasRole("ADMIN")
+            .anyExchange().authenticated(); // cualquier otra ruta debe ser con autenticacion
         }).cors(csrf -> csrf.disable())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .oauth2Login(login -> login.loginPage("/oauth2/authorization/client-app")) // nombre de la app cliente colocada en el yml
+        .oauth2Login(withDefaults())
         .oauth2Client(withDefaults())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(
             // - Obtenemos los roles del token
             // - Nos autenticamos con esos roles, en luagar de autenticarnos con los roles de scope
             // - Creamos una implementación anonima de la interfaz Converter
-                jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, AbstractAuthenticationToken>(){
+                jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, Mono<AbstractAuthenticationToken>>(){
 
                     // source: es el token
                     @Override
-                    public AbstractAuthenticationToken convert(Jwt source) {
+                    public Mono<AbstractAuthenticationToken> convert(Jwt source) {
                         Collection<String> roles = source.getClaimAsStringList("roles"); // Obteniendo el claim roles, coleccion de string
                         Collection<GrantedAuthority> authorities = roles.stream()  // Necesitamos una coleccion de GrantedAuthority/SimpleGrantedAuthority que son los roles
                             .map(SimpleGrantedAuthority::new)
                             .collect(Collectors.toList());
 
-                        return new JwtAuthenticationToken(source, authorities);
+                        return Mono.just(new JwtAuthenticationToken(source, authorities));
                     }
                     
                 })
